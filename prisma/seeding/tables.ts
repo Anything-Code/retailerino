c935f9d5a48f0c55bec2274893f68d55d4478d03
Emil-Linnebjerg <43955067+Emil-Linnebjerg@users.noreply.github.com>
2021-12-02 10:46:35 +0100

    Made seeder work
    Now only some things should be made better but it can be used to test with :)

---
+++ prisma/seeding/tables.ts
@@ -0,0 +1,241 @@
+import { List, Range } from 'immutable';
+import faker, { datatype, date, fake } from 'faker';
+import { pc } from '../../src/context';
+import { randomInt } from 'crypto';
+import { User } from '@prisma/client';
+import { InventoryGroup } from '@prisma/client';
+import { DateTime } from 'nexus-prisma/scalars';
+import { OrderI } from 'nexus-prisma/*';
+
+const amountOfCategories = 35;
+
+export function composeADate() {
+    return new Date();
+}
+
+export const rolePromises = List([
+    pc.role.create({
+        data: { name: 'admin' },
+    }),
+    pc.role.create({
+        data: { name: 'customer' },
+    }),
+]);
+
+export async function createStandaloneTables(i: number) {
+    if (i < 100) {
+        await pc.inventoryGroup.create({
+            data: {
+                itemName: faker.commerce.product(),
+                price: Number(faker.commerce.price()),
+                amount: Number(2), //faker.finance.amount()
+                displayAmount: Number(2), //some ensurance needs to be made here to ensure quantity
+                //updatedAt: '', //String(faker.time.recent())
+            },
+        });
+
+        await pc.user.create({
+            data: {
+                email: faker.internet.email(),
+                password: faker.internet.password(),
+                firstname: faker.name.firstName(),
+                lastname: faker.name.lastName(),
+                lastUserAgent: faker.internet.userAgent(),
+                phoneNumber: faker.phone.phoneNumber(),
+                role: {
+                    connect: {
+                        id: 2,
+                    },
+                },
+            },
+        });
+    }
+
+    if (i < amountOfCategories) {
+        await pc.category.create({
+            data: {
+                name: faker.commerce.department(),
+            },
+        });
+    }
+
+    if (i < 5) {
+        await pc.deliveryServiceProvider.create({
+            data: {
+                name: faker.company.companyName(),
+                pickupTime: composeADate(),
+            },
+        });
+    }
+
+    if (i < 500) {
+        await pc.image.create({
+            data: {
+                url: faker.image.imageUrl() + i,
+            },
+        });
+
+        createStandaloneTables(++i);
+    } else {
+        await createAllUserAssociations();
+        await createAllInventoryGroupAssociations();
+        await createAlloderAssociations();
+        console.info('\nSeeding was successfull!');
+        return;
+    }
+}
+
+//==========================================================================
+//======**               Change this stuff prob                     **======
+
+export function makeABarCode() {
+    return randomInt(999999);
+    // if we want to make it a real barcode we can ik this is not most essential funk
+}
+
+export function getAInventoryGroupId() {
+    return 2;
+}
+
+export function getAAdressId() {
+    return 2;
+}
+
+export function getDeliveryServiceProvicerId() {
+    return 2;
+}
+
+export function getCategoryId() {
+    return 2;
+}
+
+//==================================================================================
+//==========**         This is taking care of user and friends          **==========
+
+export async function createAllUserAssociations() {
+    const users: User[] = await pc.$queryRaw`SELECT * FROM User`;
+    users.forEach(async (user) => {
+        await reviewGenerator(user.cuid);
+        await addressGenerator(user.cuid);
+        await orderGenerator(user.cuid);
+        await cartItemGenerator(user.cuid);
+    });
+    console.info('user setup was successful');
+}
+
+async function cartItemGenerator(userCuid: string) {
+    await pc.cartItem.create({
+        data: {
+            inventoryGroupId: getAInventoryGroupId(),
+            userUId: userCuid,
+        },
+    });
+}
+
+async function reviewGenerator(userCuid: string) {
+    await pc.review.create({
+        data: {
+            inventoryGroupId: getAInventoryGroupId(),
+            userUId: userCuid,
+            description: '',
+            rating: randomInt(5),
+        },
+    });
+}
+
+async function addressGenerator(userCuid: string) {
+    await pc.address.create({
+        data: {
+            street: faker.address.streetName(),
+            city: faker.address.city(),
+            zip: 2, // no no================================================================================
+            country: faker.address.country(),
+            userUId: userCuid,
+        },
+    });
+}
+
+async function orderGenerator(userCuid: string) {
+    await pc.orderI.create({
+        data: {
+            cofirmed: true,
+            userUId: userCuid,
+            addressId: getAAdressId(),
+            deliveryServiceProvicerId: getDeliveryServiceProvicerId(),
+        },
+    });
+}
+
+//==================================================================================
+//==========**    This is taking care of inventory group and friends    **==========
+
+export async function createAllInventoryGroupAssociations() {
+    const inventoryGroupS: InventoryGroup[] = await pc.$queryRaw`SELECT * FROM InventoryGroup`;
+    let i = 0;
+    inventoryGroupS.forEach(async (inventoryGroupS) => {
+        i++;
+        await inventoryGroupCategoryGenerator(inventoryGroupS.id);
+        await inventoryGroupImageGenerator(inventoryGroupS.id, i);
+        await inventoryGroupRelationshipGenerator(inventoryGroupS.id);
+        await inventoryItemGenerator(inventoryGroupS.id, i);
+    });
+    console.info('inventory setup was successful');
+}
+
+async function inventoryGroupCategoryGenerator(inventoryGroupIdl: number) {
+    await pc.inventoryGroupCategory.create({
+        data: {
+            inventoryGroupId: inventoryGroupIdl,
+            categoryId: getCategoryId(),
+        },
+    });
+}
+
+async function inventoryGroupImageGenerator(inventoryGroupIdl: number, i: number) {
+    await pc.inventoryGroupImage.create({
+        data: {
+            inventoryGroupId: inventoryGroupIdl,
+            imageId: i,
+        },
+    });
+}
+
+async function inventoryGroupRelationshipGenerator(inventoryGroupIdl: number) {
+    await pc.inventoryGroupRelationship.create({
+        data: {
+            inventoryGroupIdFrom: inventoryGroupIdl,
+            inventoryGroupIdTo: getAInventoryGroupId(),
+        },
+    });
+}
+
+async function inventoryItemGenerator(inventoryGroupIdl: number, i: number) {
+    await pc.inventoryItem.create({
+        data: {
+            uBarcode: makeABarCode(),
+            note: faker.commerce.productDescription(),
+            inventoryGroupId: inventoryGroupIdl,
+            arrivedAt: composeADate(),
+            shippedAt: composeADate(),
+            soldAt: composeADate(),
+        },
+    });
+}
+
+//==================================================================================
+//==========**               This is taking care of oders               **==========
+
+export async function createAlloderAssociations() {
+    const orderIdS: OrderI[] = await pc.$queryRaw`SELECT * FROM OrderI`;
+    let i = 0;
+    orderIdS.forEach(async (orderIdl) => {
+        await pc.orderItem.create({
+            data: {
+                inventoryGroupId: getAInventoryGroupId(),
+                orderId: Number(orderIdl.id),
+            },
+        });
+    });
+    console.info('order setup was successful');
+}
+//yarn seed, reset, push
